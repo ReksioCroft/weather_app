7b8ced807eb14754ecc6c62306ec209f94327524
reksio <octavian.staicu@s.unibuc.ro>
2021-07-07 21:30:47 +0300

    model, data, action, middleware, reducer

---
+++ lib/src/middleware/middleware.dart
@@ -0,0 +1,27 @@
+import 'package:redux/redux.dart';
+import 'package:weather_app/src/actions/get_location.dart';
+import 'package:weather_app/src/data/location_api.dart';
+import 'package:weather_app/src/models/app_state.dart';
+import 'package:weather_app/src/models/location.dart';
+
+class AppMiddleware {
+  AppMiddleware(this._locationApi);
+
+  final LocationApi _locationApi;
+
+  List<Middleware<AppState>> get middleware {
+    return <Middleware<AppState>>[
+      TypedMiddleware<AppState, GetLocation>(_getLocation),
+    ];
+  }
+
+  Future<void> _getLocation(Store<AppState> store, GetLocation action, NextDispatcher next) async {
+    next(action);
+    try {
+      final Location location = await _locationApi.getLocation();
+      store.dispatch(GetLocationSuccessful(location));
+    } catch (error) {
+      store.dispatch(GetLocationError(error));
+    }
+  }
+}
---
+++ lib/src/reducer/reducer.dart
@@ -0,0 +1,13 @@
+import 'package:redux/redux.dart';
+import 'package:weather_app/src/actions/get_location.dart';
+import 'package:weather_app/src/models/app_state.dart';
+
+Reducer<AppState> reducer = combineReducers(<Reducer<AppState>>[
+  TypedReducer<AppState, GetLocationSuccessful>(_getLocationSuccessful),
+]);
+
+AppState _getLocationSuccessful(AppState state, GetLocationSuccessful action) {
+  return state.rebuild((b) {
+    b.location = action.location.toBuilder();
+  });
+}
